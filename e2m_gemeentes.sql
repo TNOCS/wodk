bd2776efb6c489b3d714dcc9a7bb17cd3d87e628
Rinze <rinze.bruining@tno.nl>
2016-09-29 10:52:17 +0200

    Update provincie and gemeente overviews. Add 'aanname' to lift/souterrain.

---
+++ e2m_gemeentes.sql
@@ -0,0 +1,3 @@
+﻿select ster_0,ster_1,ster_2,ster_3,ster_4,ster_5,ster_onb,ster_in_ond,ster_totaal,a_huko_onb,a_koopwon,
+a_huurwon,a_1gezw,a_mgezw,concat('GM',lpad(gemeentecode::text, 4, '0')) as GM_CODE,gemeentenaam,aant_inw
+from bagactueel.gemeente
---
+++ e2m_provincies.sql
@@ -0,0 +1,3 @@
+﻿select ster_0,ster_1,ster_2,ster_3,ster_4,ster_5,ster_onb,ster_in_ond,ster_totaal,
+a_huko_onb,a_koopwon,a_huurwon,a_1gezw,a_mgezw,aant_inw,provincienaam
+from bagactueel.provincie
